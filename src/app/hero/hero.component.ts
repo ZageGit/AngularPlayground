96ab43018d0f5d1628b3231a04ab3ce571989542
Johannes Zarre <zarre.johannes@gmail.com>
2020-10-19 16:54:43 +0200

    right status now

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+  heroHeading: string;
+  heroText: string;
+  heroBtnText : string;
+  heroBtnUrl:   string ;
+
+  constructor() { 
+    this.heroHeading =  "My first own Angular App" ;
+
+  this.heroText =  "This is the first time i am using Angular and Typescript alone by myself to learn it because i really like it and i want to learn a lot about all of thi shit" ;
+
+  this.heroBtnText =  "Learn more" ;
+
+  this.heroBtnUrl =  "https://codefactory.wien"  ;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
